4ef155278ff17b51868bdfaff52fd03a165bcc80
Chithra42 <chithraaneetha@gmail.com>
2021-12-11 16:50:14 +0530

    First commit

--- Front End/src/app/app-routing.module.ts
+++ Front End/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './books/books.component';
+import { LoginComponent } from './login/login.component';
 import { NewBookComponent } from './new-book/new-book.component';
+
 const routes: Routes = [
   {path:"",component:BooksComponent},
-  {path:"addbooks",component:NewBookComponent}
+  {path:"addbooks",component:NewBookComponent},
+  {path:"login",component:LoginComponent}
 ];
 
 @NgModule({
